66698f030b4261d8b6cef608342a0aecdf973127
No1awesone <Fardinali178@gmail.com>
2026-09-10 14:34:20 +0100

    Refactor HTML structure and fix CSS border property; add initial JavaScript data structure for Kanban board

---
+++ script.js
@@ -0,0 +1,28 @@
+
+//data 
+const columns = [
+  {
+    id: "todo",
+    title: "To Do",
+    cards: [
+      {
+        id: 1,
+        title: "Learn JavaScript"
+      }
+    ]
+  },
+
+  {
+    id: "in-progress",
+    title: "In Progress",
+    cards: []
+  },
+
+  {
+    id: "done",
+    title: "Done",
+    cards: []
+  }
+];
+
+
